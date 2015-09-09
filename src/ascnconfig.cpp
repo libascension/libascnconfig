894c9f453596b2ad5ed811d5790645fb73046206
Paul Bunyan <hal7df@gmail.com>
2015-09-09 13:33:38 -0400

    Added a ConfigItem object and basic initialization for ascnconfig.
    - Configuration items are to be stored in a ConfigItem class that wraps
    around a union holding all of the possible setting value types

---
+++ src/ascnconfig.cpp
@@ -0,0 +1,81 @@
+#include "ascnconfig.h"
+using namespace config;
+
+/** SINGLE CONFIGURATION ITEM **/
+
+ConfigItem::ConfigItem()
+{
+
+}
+
+ConfigItem::ConfigItem(std::string contents)
+{
+	m_content.s = contents;
+}
+
+ConfigItem::ConfigItem(int contents)
+{
+	m_content.i = contents;
+}
+
+ConfigItem::ConfigItem(double contents)
+{
+	m_content.r = contents;
+}
+
+ConfigItem::ConfigItem(bool contents)
+{
+	m_content.b = contents;
+}
+
+std::string ConfigItem::getString()
+{
+	return m_content.s;
+}
+
+int ConfigItem::getInt()
+{
+	return m_content.i;
+}
+
+double ConfigItem::getReal()
+{
+	return m_content.r;
+}
+
+bool ConfigItem::getBool()
+{
+	return m_content.b;
+}
+
+/** CONFIG OBJECT **/
+ascnconfig::ascnconfig (std::string appName, bool importFromFile)
+{
+	m_app = appName;
+	m_error = kNone;
+
+	if (importFromFile)
+	{
+		std::stringstream ss;
+
+		ss << paths::getConfigPath() << '/' << m_app << ".conf";
+
+		importSettings(ss.str());
+	}
+}
+
+ascnconfig::ascnconfig (std::string orgName, std::string appName, bool importFromFile)
+{
+	m_org = orgName;
+	m_app = appName;
+	m_error = kNone;
+
+	if (importFromFile)
+	{
+		std::stringstream ss;
+
+		ss << paths::getConfigPath() << '/' << m_org << '/' << m_app << ".conf";
+
+		importSettings(ss.str());
+	}
+}
--- src/ascnconfig.h
+++ src/ascnconfig.h
@@ -3,10 +3,45 @@
 
 #include <fstream>
 #include <string>
+#include <sstream>
 #include <map>
 #include "ascnpaths.h"
 
 namespace config {
+
+	enum configError_t {
+		kNone,
+		kKeyNotFound,
+		kInvalidKey,
+		kUnknownKeyError,
+		kFileNotFound
+	};
+
+	union configItem_u {
+		std::string s;
+		int i;
+		double r;
+		bool b;
+	};
+
+	class ConfigItem {
+	public:
+		ConfigItem();
+		ConfigItem(std::string contents);
+		ConfigItem(int contents);
+		ConfigItem(double contents);
+		ConfigItem(bool contents);
+
+		~ConfigItem();
+
+		std::string getString();
+		int getInt();
+		double getReal();
+		bool getBool();
+	private:
+		configItem_u m_content;
+	};
+
 	class ascnconfig {
 	public:
 		ascnconfig(std::string appName, bool importFromFile=false);
@@ -20,12 +55,25 @@ namespace config {
 		bool saveConfig(std::string filename);
 
 		/** CONFIG API **/
-		std::string getValue(std::string key);
-		int getValue(std::string key);
-		bool getValue(std::string key);
+		ConfigItem getValue(std::string key);
+
+		bool setValue(std::string key, std::string value);
+		bool setValue(std::string key, int value);
+		bool setValue(std::string key, bool value);
+
+		/** CONFIGURE CONFIGURATION OBJECT (hehe) **/
+		void setAppName(std::string appName);
+		void setOrgName(std::string orgName);
+		void setAppQualified(std::string orgName, std::string appName);
 
 	private:
+		bool importSettings(std::string fname);
+		bool exportSettings(std::string fname);
+		std::map<std::string,ConfigItem*> m_settings;
 
+		std::string m_org, m_app;
+
+		configError_t m_error;
 	};
 }
 
